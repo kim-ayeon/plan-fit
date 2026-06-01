f742bc0b496ab4a2a3d56b2c9a9530600f9ec706
kim-ayeon <limebaekjak@gmail.com>
2026-06-01 11:56:16 +0900

    Create data.js

---
+++ app/data.js
@@ -0,0 +1,835 @@
+window.PLANFIT_DATA = {
+ "major": [
+  {
+   "name": "행정학개론",
+   "credits": 3,
+   "category": "전공기초",
+   "required": true,
+   "year": 1,
+   "sem": 1
+  },
+  {
+   "name": "정책학개론",
+   "credits": 3,
+   "category": "전공기초",
+   "required": true,
+   "year": 1,
+   "sem": 2
+  },
+  {
+   "name": "재무행정론",
+   "credits": 3,
+   "category": "전공기초",
+   "required": false,
+   "year": 2,
+   "sem": 1
+  },
+  {
+   "name": "인사행정론",
+   "credits": 3,
+   "category": "전공기초",
+   "required": false,
+   "year": 2,
+   "sem": 1
+  },
+  {
+   "name": "지방자치론",
+   "credits": 3,
+   "category": "전공기초",
+   "required": false,
+   "year": 2,
+   "sem": 2
+  },
+  {
+   "name": "비교행정론",
+   "credits": 3,
+   "category": "전공기초",
+   "required": false,
+   "year": 2,
+   "sem": 2
+  },
+  {
+   "name": "계량행정과 데이터분석",
+   "credits": 3,
+   "category": "전공기초",
+   "required": false,
+   "year": 2,
+   "sem": 1
+  },
+  {
+   "name": "사회정책론",
+   "credits": 3,
+   "category": "전공기초",
+   "required": false,
+   "year": 2,
+   "sem": 2
+  },
+  {
+   "name": "행정과 사회",
+   "credits": 3,
+   "category": "전공핵심",
+   "required": false,
+   "year": 1,
+   "sem": 1
+  },
+  {
+   "name": "한국정부론",
+   "credits": 3,
+   "category": "전공핵심",
+   "required": false,
+   "year": 1,
+   "sem": 2
+  },
+  {
+   "name": "행정조직론",
+   "credits": 3,
+   "category": "전공핵심",
+   "required": true,
+   "year": 2,
+   "sem": 1
+  },
+  {
+   "name": "미래사회정부혁신",
+   "credits": 3,
+   "category": "전공핵심",
+   "required": false,
+   "year": 2,
+   "sem": 2
+  },
+  {
+   "name": "사회조사방법론",
+   "credits": 3,
+   "category": "전공핵심",
+   "required": false,
+   "year": 2,
+   "sem": 1
+  },
+  {
+   "name": "지방재정론",
+   "credits": 3,
+   "category": "전공핵심",
+   "required": false,
+   "year": 2,
+   "sem": 2
+  },
+  {
+   "name": "글로벌행정과커뮤니케이션",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 1
+  },
+  {
+   "name": "정부부패론",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 1
+  },
+  {
+   "name": "행정&정책이론",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 1
+  },
+  {
+   "name": "공공기관론",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 1
+  },
+  {
+   "name": "ICT 디지털 정부",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 1
+  },
+  {
+   "name": "공직자를 위한 법률 지식과 역량 1",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 1
+  },
+  {
+   "name": "도시정책론",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 2
+  },
+  {
+   "name": "인간관계와리더십",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 2
+  },
+  {
+   "name": "정부와 기업",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 2
+  },
+  {
+   "name": "공공마케팅과 PR",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 2
+  },
+  {
+   "name": "공직자를 위한 법률 지식과 역량 2",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 2
+  },
+  {
+   "name": "지방의회론",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 3,
+   "sem": 2
+  },
+  {
+   "name": "행정실무와 사례연구",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 4,
+   "sem": 1
+  },
+  {
+   "name": "정책분석론",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 4,
+   "sem": 1
+  },
+  {
+   "name": "창의적종합설계",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 4,
+   "sem": 1
+  },
+  {
+   "name": "재정관리론",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 4,
+   "sem": 1
+  },
+  {
+   "name": "행정 기획과 커뮤니케이션",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 4,
+   "sem": 2
+  },
+  {
+   "name": "정책집행&평가론",
+   "credits": 3,
+   "category": "전공심화",
+   "required": false,
+   "year": 4,
+   "sem": 2
+  }
+ ],
+ "gen": [
+  {
+   "name": "비판적사고와소통",
+   "credits": 3,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "CommunicationinEnglish(L＆R)Ⅰ",
+   "credits": 2,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "CommunicationinEnglish(L＆R)Ⅱ",
+   "credits": 2,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "CommunicationinEnglish(S&W)Ⅰ",
+   "credits": 2,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "CommunicationinEnglish(S&W)Ⅱ",
+   "credits": 2,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "한국어쓰기와발표",
+   "credits": 3,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "한국어문규정",
+   "credits": 2,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "한국어문화의이해",
+   "credits": 2,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "SW와문제해결",
+   "credits": 2,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "컴퓨팅적사고",
+   "credits": 2,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "대학생활과미래설계",
+   "credits": 1,
+   "category": "교양필수",
+   "balance": ""
+  },
+  {
+   "name": "인공지능과윤리적삶",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "영미청소년소설읽기",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "사랑과죽음의문학적탐구",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "시민교육론",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "청소년교육론",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "인간심리의이해",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "통계로보는인간의마음",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "지역문제해결학습",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "영화와함께하는심리여행",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "미래사회에서교육과수업의혁신",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "4차산업혁명시대영어학습-디지털리터러시-",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "공학윤리",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "텍스트와함께철학하기",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "윤리와시민",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "서양의역사와문화",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "아시아의역사와문화",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "아틀라스세계여행(AtlasWorldTour)",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "아틀라스한국여행",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "연구윤리와사회적책임",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "4차산업혁명의창의적융합인재양성",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "글로벌이슈와관광",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "공공부문진출을위한행정기초지식",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "언어와사회",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "교양으로배우는셰익스피어",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "미국문화이해",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "A"
+  },
+  {
+   "name": "데이터분석과인공지능활용",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "웹기반수업의이론과교수전략",
+   "credits": 2,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "기후변화상식을넘어서",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "컴퓨터의활용",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "AI리터러시",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "통계의이해와활용",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "과학의역사",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "환경과생태",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "디지털포토그래피",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "일반생명과학",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "코딩연습",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "창의적프로그램코딩기초",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "R프로그래밍",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "발명과특허",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "건축의이해",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "컴퓨터프로그래밍입문",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "4차산업혁명시대의빅데이터",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "컴퓨터와인터넷",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "상상설계",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "인간지능과인공지능",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "스마트생명과학실험",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "생명과학의이해",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "생화학",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "생명과학빅데이터의이해",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "동물과자원",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "B"
+  },
+  {
+   "name": "기초영어",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "마음공부와명상",
+   "credits": 2,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "K-POP과판소리",
+   "credits": 2,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "요가와필라테스",
+   "credits": 2,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "토익Ⅰ",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "영어발음클리닉",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "식생활트렌드와건강",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "배드민턴초급",
+   "credits": 2,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "야구&소프트볼",
+   "credits": 2,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "기초골프",
+   "credits": 2,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "현대생활과디자인",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "국악연주의기초",
+   "credits": 2,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "영문독해",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "여행중국어",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "중국어첫걸음",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "영어핵심구문12가지레시피",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "초급영어회화",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "기초독일어",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "문화산업과문화콘텐츠",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "국제비즈니스와무역영어",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "패션이미지스타일링",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "환경과패션",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "디자인씽킹",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "기초드로잉",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  },
+  {
+   "name": "영양,수면,빛과생체리듬",
+   "credits": 3,
+   "category": "교양선택",
+   "balance": "C"
+  }
+ ],
+ "req": {
+  "total": 130,
+  "majorRequired": [
+   "행정학개론",
+   "정책학개론",
+   "행정조직론"
+  ],
+  "majorMin": 66,
+  "genReq": 24,
+  "genBalanceMin": 12,
+  "genBalanceAreas": 2
+ },
+ "jobs": {
+  "공무원/공공기관": [
+   "행정",
+   "정책",
+   "정부",
+   "공공",
+   "재무",
+   "인사",
+   "지방자치",
+   "법률",
+   "기획",
+   "조직",
+   "재정",
+   "공직"
+  ],
+  "데이터분석": [
+   "데이터",
+   "계량",
+   "분석",
+   "통계",
+   "ICT",
+   "디지털",
+   "조사방법",
+   "인공지능",
+   "컴퓨팅"
+  ],
+  "기업 일반/경영": [
+   "기업",
+   "마케팅",
+   "PR",
+   "리더십",
+   "커뮤니케이션",
+   "경영",
+   "관계",
+   "혁신"
+  ],
+  "글로벌/국제": [
+   "글로벌",
+   "국제",
+   "비교",
+   "커뮤니케이션",
+   "영어",
+   "외국어"
+  ]
+ }
+};
